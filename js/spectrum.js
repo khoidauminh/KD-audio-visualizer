10357f05a7812f4961e63c00ba72ecb8c74e7645
KD <81458575+khoidauminh@users.noreply.github.com>
2021-08-04 18:28:53 +0700

    Update spectrum.js

--- js/spectrum.js
+++ js/spectrum.js
@@ -1,23 +1,64 @@
-// The spectrum will shrink the audio stream by conbining 2 halves of them together. 
-// Refresh rate is put on front.
+// The spectrum will shrink the audio stream by conbining 2 halves of them together. Speed is put prior to accuration.
+// The complex part of the output is ignored, which means the complex rotaion of the equation is direction-independent on the real axis, and therefore, the minus sign of -2πif can be removed.
 
 function getFreqDomain(buffer, freqMin = 0, freqMax = 200000, freqReso = 1024) {
+	return fourierTransform(buffer, freqMin, freqMax, freqReso);
+}
+
+function fourierTransform(buffer, freqMin = 0, freqMax = 200000, freqReso = 1024) {
 	const 
 	bufferLengthH = buffer.length/2;
 	
 	var 
-	freqTable = [];
+	freqTable = [],
+	combinedBuffer = [];
+	
+	for (var i = 0 ; i < bufferLengthH ; i++) {
+		combinedBuffer.push((buffer[i] + buffer[i+bufferLengthH])/2);
+	}
 	
 	for (var currentBand = 0; currentBand < freqReso ; currentBand++) {
 		const 
-		bandFreq = map(currentBand, 0, freqReso, freqMin, freqMax),
-		revolutions = bandFreq*Math.PI;
+		bandFreq = (currentBand/freqReso)*(freqMax-freqMin) + freqMin,
+		revolutions = bandFreq*pi2;
 		
 		var
 		weight = 0;
 				
 		for (var sample = 0 ; sample < bufferLengthH ; sample++) {
-			weight += Math.cos(-revolutions*(sample/bufferLengthH))*(buffer[sample] + buffer[sample+bufferLengthH])/2;
+			weight += Math.cos(revolutions*(sample/bufferLengthH))*combinedBuffer[sample];
+		}
+		//weight /= bufferLength;
+		
+		freqTable.push(weight);
+	}
+	
+	return freqTable;
+}
+
+// ft but with diferent wave functions.
+
+function ft_nonSineW(buffer, freqMin = 0, freqMax = 200000, freqReso = 1024) {
+	const bufferLengthH = buffer.length;
+	
+	var 
+	freqTable = [],
+	combinedBuffer = [];
+	
+	for (var i = 0 ; i < bufferLengthH ; i++) {
+		combinedBuffer.push((buffer[i] + buffer[i+bufferLengthH])/2);
+	}
+	
+	for (var currentBand = 0; currentBand < freqReso ; currentBand++) {
+		const 
+		bandFreq = (currentBand/freqReso)*(freqMax-freqMin) + freqMin,
+		revolutions = bandFreq*pi2;
+		
+		var
+		weight = 0;
+				
+		for (var sample = 0 ; sample < bufferLengthH ; sample += 2) {
+			weight += trianglew(revolutions*(sample/bufferLengthH))*combinedBuffer[sample];
 		}
 		//weight /= bufferLength;
 		
